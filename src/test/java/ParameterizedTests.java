6da2b8678284991fa6318fab79932615c656eccc
McOwska <zuza.m.makowska@gmail.com>
2022-10-31 21:10:45 +0100

    parameterized test with enum source

--- src/test/java/ParameterizedTests.java
+++ src/test/java/ParameterizedTests.java
@@ -1,11 +1,17 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ParameterizedTests {
+
+    private enum Operation {
+        ADD, SUBTRACT, MULTIPLY, DIVIDE
+    }
 
     private Calculator calculator;
 
@@ -24,6 +30,12 @@ public class ParameterizedTests {
     @CsvSource({"1.0, 2.0", "3.0, 4.0"})
     void ParametrizedAddMethodTest(double param1, double param2){
         assertEquals(param1 + param2, calculator.add(param1, param2));
+    }
+
+    @ParameterizedTest
+    @EnumSource(Operation.class)
+    void ParametrizedEnumTest(Operation operation){
+        System.out.println(operation);
     }
 
 
