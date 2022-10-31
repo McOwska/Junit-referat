2ce502bdb8ebd283a76dddea6168d92896b8043f
McOwska <92335309+McOwska@users.noreply.github.com>
2022-10-31 14:44:53 +0100

    Add files via upload
    testy parametryzowane

---
+++ src/test/java/ParameterizedTests.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ParameterizedTests {
+    @ParameterizedTest
+    @CsvSource({"1,2", "3,4"})
+    void ParametrizedAddMethodTest(int param1, int param2){
+        Calculator calculator = new Calculator();
+        assertEquals(param1 + param2, calculator.add(param1, param2));
+    }
+}
